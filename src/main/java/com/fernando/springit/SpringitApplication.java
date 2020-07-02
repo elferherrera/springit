a3b77477dfb3589032cf087cd6ab6d4b5015bf1a
Fernando José Herrera Elizalde <fernandoj.herrera@softtek.com>
2020-07-02 15:46:26 -0500

    Change files

--- src/main/java/com/fernando/springit/SpringitApplication.java
+++ src/main/java/com/fernando/springit/SpringitApplication.java
@@ -3,6 +3,7 @@ package com.fernando.springit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringitApplication {
 
