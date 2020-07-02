27ca5fae33f8f5eb6dc140fba65da4f07d4cf7dd
Fernando José Herrera Elizalde <fernandoj.herrera@softtek.com>
2020-07-02 17:56:14 -0500

    Added entities with lombok

---
+++ src/main/java/com/fernando/springit/domain/Comment.java
@@ -0,0 +1,22 @@
+package com.fernando.springit.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String body;
+
+    // link
+}
---
+++ src/main/java/com/fernando/springit/domain/Vote.java
@@ -0,0 +1,23 @@
+package com.fernando.springit.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Vote {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private int vote;
+
+    // user
+    // link
+}
